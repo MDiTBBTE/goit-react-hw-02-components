d001f75c713752576aa97fb8d3b520a6309ced84
mditbbte <andrii.ds.kor@gmail.com>
2019-11-30 06:26:31 +0200

    Updates

---
+++ precache-manifest.c37c7e416b2c41bf4571619ca7cc0a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1386c8bbda6397c8c80d013dabf6544d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e15c6fba1bc28eac7c63",
+    "url": "/static/css/main.91043201.chunk.css"
+  },
+  {
+    "revision": "8892993cf548dcc60ad2",
+    "url": "/static/js/2.f36d4e3f.chunk.js"
+  },
+  {
+    "revision": "e15c6fba1bc28eac7c63",
+    "url": "/static/js/main.24e8c0f9.chunk.js"
+  },
+  {
+    "revision": "bac507b99add4be59f1f",
+    "url": "/static/js/runtime-main.c1c97c11.js"
+  }
+]);
